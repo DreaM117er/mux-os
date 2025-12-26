d9531424187712df4d8bfc7dcd675a704317011e
DreaM117er <m1223961117@gmail.com>
2025-12-27 04:24:49 +0800

    update 2.1.0

--- ui.sh
+++ ui.sh
@@ -216,9 +216,9 @@ function _mux_fuzzy_menu() {
         --layout=reverse \
         --border=bottom \
         --prompt=" :: Neural Link › " \
-        --header=" [Active Protocol Slots: 6/$total_cmds]" \
+        --header=":: Active Protocol Slots: 6/$total_cmds]" \
         --info=hidden \
-        --pointer="››" \
+        --pointer=" ››" \
         --color=fg:white,bg:-1,hl:green,fg+:cyan,bg+:black,hl+:yellow,info:yellow,prompt:cyan,pointer:red,border:blue \
         --bind="resize:clear-screen"
     )
