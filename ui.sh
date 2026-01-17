e867cadfaadfe6c484c630976c6ad18eb56dcb72
DreaM117er <m1223961117@gmail.com>
2026-01-18 05:05:35 +0800

    back to stable fzf

--- ui.sh
+++ ui.sh
@@ -640,12 +640,12 @@ function _factory_fzf_cat_selector() {
             name=$3; gsub(/^"|"$/, "", name)
 
             if (!seen[id]++) {
-                printf "%3d|%s\n", id, name
+                printf "%03d|%s\n", id, name
             }
         }
     ' "$target_file" | sort -n)
 
-    local selected=$(echo "$cat_list" | awk -F'|' '{printf " \033[1;33m[%03d]  \033[1;37m%s\n", $1, $2}' | fzf --ansi \
+    local selected=$(echo "$cat_list" | awk -F'|' '{printf " \033[1;33m%03d  \033[1;37m%s\n", $1, $2}' | fzf --ansi \
         --height=10 \
         --layout=reverse \
         --border=bottom \
@@ -701,7 +701,7 @@ function _factory_fzf_cmd_in_cat() {
         --border=bottom \
         --info=hidden \
         --prompt=" :: Select Command › " \
-        --header=" :: Category [$target_cat_no] : [$total] Items :: " \
+        --header=" :: Category: [$target_cat_no:$total] :: " \
         --pointer="››" \
         --color=fg:white,bg:-1,hl:240,fg+:white,bg+:235,hl+:240 \
         --color=info:240,prompt:208,pointer:red,marker:208,border:208,header:240 \
