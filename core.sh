c2f75d77965d1c4a82195c8ce7477545b5011398
DreaM117er <m1223961117@gmail.com>
2026-02-02 14:27:40 +0800

    fix factory boot

--- core.sh
+++ core.sh
@@ -39,6 +39,24 @@ else
     MUX_STATUS="DEFAULT"
 fi
 
+# 系統輸入鎖定與解鎖
+function _system_lock() {
+    if [ -t 0 ]; then stty -echo; fi
+}
+
+function _system_unlock() {
+    if [ -t 0 ]; then stty echo; fi
+}
+
+# 無參數檢測輔助函式
+function _require_no_args() {
+    if [ -n "$1" ]; then
+        _bot_say "no_args" "Unexpected input: $*"
+        return 1
+    fi
+    return 0
+}
+
 case "$MUX_MODE" in
     "FAC")
         if [ -f "$MUX_ROOT/factory.sh" ]; then
@@ -51,7 +69,7 @@ case "$MUX_MODE" in
             else
                 echo -e "\033[1;31m :: FATAL :: Factory Core Not Found.\033[0m"
             fi
-            
+
             return 0 2>/dev/null || exit 0
         fi
         ;;
@@ -299,24 +317,6 @@ function _mux_init() {
     export MUX_INITIALIZED="true"
     _system_unlock
     _bot_say "hello"
-}
-
-# 無參數檢測輔助函式
-function _require_no_args() {
-    if [ -n "$1" ]; then
-        _bot_say "no_args" "Unexpected input: $*"
-        return 1
-    fi
-    return 0
-}
-
-# 系統輸入鎖定與解鎖
-function _system_lock() {
-    if [ -t 0 ]; then stty -echo; fi
-}
-
-function _system_unlock() {
-    if [ -t 0 ]; then stty echo; fi
 }
 
 # 安全介面寬度計算
