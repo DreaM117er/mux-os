da50970f8812c147880e12a0a782ef6fcfbc6cf9
DreaM117er <m1223961117@gmail.com>
2026-02-20 22:38:52 +0800

    add new mode

--- core.sh
+++ core.sh
@@ -1806,7 +1806,7 @@ case "$MUX_MODE" in
         if [ -f "$MUX_ROOT/eroc.sh" ]; then
             source "$MUX_ROOT/eroc.sh"
             
-            export PS1="\[${C_TAVIOLET}\]Mux\[${C_RESET}\] \w \033[5m›\033[0m "
+            export PS1="\[${C_TAVIOLET}\]Xum\[${C_RESET}\] \w \033[5m›\033[0m "
             
             if command -v _xum_system_boot &> /dev/null; then
                 _xum_system_boot
