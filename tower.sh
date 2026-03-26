fb9da249c5c68eb546333ed96a87e5345b3c54c9
DreaM117er <m1223961117@gmail.com>
2026-03-26 14:28:35 +0800

    fix cmt core

--- tower.sh
+++ tower.sh
@@ -1324,8 +1324,8 @@ function __tct_core() {
     local rand_chance=$(( RANDOM % 100 ))
 
     if [ "$MUX_MODE" == "MUX" ]; then
-        if [ "$current_level" -ge 8 ] && [ "$rand_chance" -lt 60 ]; then
-            if [ "$MUX_STATUS" == "DEFAULT" ]; then
+        if [ "$current_level" -ge 8 ]; then
+            if [ "$MUX_STATUS" == "DEFAULT" ] && [ "$rand_chance" -lt 60 ]; then
                 echo -e "${C_WHITE} :: OK, it's time to login the Command Tower gate now.${C_RESET}"
             else
                 echo -e "${C_WHITE} :: I need to back to Hanger first.${C_RESET}"
